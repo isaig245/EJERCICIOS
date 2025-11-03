1b87f959926710e5acdbb53fec69173582618fcb
isaig245 <isaireyes@unialvart.edu.mx>
2025-11-03 08:41:56 -0600

    Create if_solo.c

---
+++ if_solo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    
+int edad = 11;
+
+if (edad >= 18) {
+    printf("Eres mayor de edad.\n");
+}
+printf("edad %d .\n", edad); 
+// Output: Fin del programa. (No se mostró el mensaje del if)
+    return 0;
+}
